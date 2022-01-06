11e06a1f6c511e0e620d2f0021e595d7f46e8db8
LaptopAZ.vn <hoanghoa97hb@gmail.com>
2022-01-06 16:29:58 +0700

    no message

--- app/src/main/java/com/palfs/cameraxinjava/MainActivity.java
+++ app/src/main/java/com/palfs/cameraxinjava/MainActivity.java
@@ -1,5 +1,16 @@
 package com.palfs.cameraxinjava;
 
+import android.Manifest;
+import android.annotation.SuppressLint;
+import android.content.ContentValues;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+import android.util.Size;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
@@ -14,38 +25,18 @@ import androidx.camera.lifecycle.ProcessCameraProvider;
 import androidx.camera.view.PreviewView;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
-import androidx.core.content.FileProvider;
 import androidx.lifecycle.LifecycleOwner;
-
-import android.Manifest;
-import android.annotation.SuppressLint;
-import android.content.ContentValues;
-import android.content.Context;
-import android.content.pm.PackageManager;
-import android.net.Uri;
-import android.os.Build;
-import android.os.Bundle;
-import android.os.Environment;
-import android.os.Handler;
-import android.provider.MediaStore;
-import android.util.Log;
-import android.util.Size;
-import android.view.Surface;
-import android.view.View;
-import android.widget.Button;
-import android.widget.Toast;
 
 import com.google.common.util.concurrent.ListenableFuture;
 
-import java.io.File;
-import java.net.URI;
-import java.util.Date;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.Executor;
 
 public class MainActivity extends AppCompatActivity implements ImageAnalysis.Analyzer, View.OnClickListener {
+    private static final int CAMERA_PERMISSION_CODE = 10;
     private ListenableFuture<ProcessCameraProvider> cameraProviderFuture;
-    private static final String[] CAMERA_PERMISSION = new String[]{Manifest.permission.CAMERA};
+    private static final String[] CAMERA_PERMISSION = new String[]{Manifest.permission.CAMERA
+            ,Manifest.permission.RECORD_AUDIO,Manifest.permission.WRITE_EXTERNAL_STORAGE};
     private static final int CAMERA_REQUEST_CODE = 10;
     PreviewView previewView;
     private ImageCapture imageCapture;
@@ -67,17 +58,17 @@ public class MainActivity extends AppCompatActivity implements ImageAnalysis.Ana
         bRecord.setOnClickListener(this);
         if (!hasCameraPermission()) {
             requestPermission();
+        }else {
+            cameraProviderFuture = ProcessCameraProvider.getInstance(this);
+            cameraProviderFuture.addListener(() -> {
+                try {
+                    ProcessCameraProvider cameraProvider = cameraProviderFuture.get();
+                    startCameraX(cameraProvider);
+                } catch (ExecutionException | InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }, getExecutor());
         }
-        cameraProviderFuture = ProcessCameraProvider.getInstance(this);
-        cameraProviderFuture.addListener(() -> {
-            try {
-                ProcessCameraProvider cameraProvider = cameraProviderFuture.get();
-                startCameraX(cameraProvider);
-            } catch (ExecutionException | InterruptedException e) {
-                e.printStackTrace();
-            }
-        }, getExecutor());
-
     }
 
     Executor getExecutor() {
@@ -110,6 +101,25 @@ public class MainActivity extends AppCompatActivity implements ImageAnalysis.Ana
                 .build();
 
         imageAnalysis.setAnalyzer(getExecutor(), this);
+
+
+//        ImageAnalysis imageAnalysis = new ImageAnalysis.Builder()
+//                .setTargetResolution(new Size(1280, 720))
+//                .setBackpressureStrategy(ImageAnalysis.STRATEGY_KEEP_ONLY_LATEST).build();
+//        imageAnalysis.setAnalyzer(ContextCompat.getMainExecutor(this), new ImageAnalysis.Analyzer() {
+//
+//            @Override
+//            public void analyze(@NonNull ImageProxy image) {
+//                Log.e("hubert", "Helllo anh emmmmmmmm");
+//                Log.e("hubert", " " + image.getPlanes()[0].getBuffer());
+////                image.getPlanes()[0].buffer[1]: red
+////                image.getPlanes()[0].buffer[2]: green
+////                image.getPlanes()[0].buffer[3]: blue
+//
+//                image.close();
+//            }
+//        });
+
 
         //bind to lifecycle:
         cameraProvider.bindToLifecycle((LifecycleOwner) this, cameraSelector, preview, imageCapture, videoCapture);
@@ -157,6 +167,31 @@ public class MainActivity extends AppCompatActivity implements ImageAnalysis.Ana
         );
     }
 
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == CAMERA_PERMISSION_CODE) {
+
+            // Checking whether user granted the permission or not.
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                cameraProviderFuture = ProcessCameraProvider.getInstance(this);
+                cameraProviderFuture.addListener(() -> {
+                    try {
+                        ProcessCameraProvider cameraProvider = cameraProviderFuture.get();
+                        startCameraX(cameraProvider);
+                    } catch (ExecutionException | InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }, getExecutor());
+                // Showing the toast message
+                Toast.makeText(MainActivity.this, "Camera Permission Granted", Toast.LENGTH_SHORT).show();
+            }
+            else {
+                Toast.makeText(MainActivity.this, "Camera Permission Denied", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
     @SuppressLint("RestrictedApi")
     private void recordVideo() {
         if (videoCapture != null) {
@@ -174,26 +209,27 @@ public class MainActivity extends AppCompatActivity implements ImageAnalysis.Ana
                     //                                          int[] grantResults)
                     // to handle the case where the user grants the permission. See the documentation
                     // for ActivityCompat#requestPermissions for more details.
+                    return;
                 }
-                videoCapture.startRecording(
-                        new VideoCapture.OutputFileOptions.Builder(
-                                getContentResolver(),
-                                MediaStore.Video.Media.EXTERNAL_CONTENT_URI,
-                                contentValues
-                        ).build(),
-                        getExecutor(),
-                        new VideoCapture.OnVideoSavedCallback() {
-                            @Override
-                            public void onVideoSaved(@NonNull VideoCapture.OutputFileResults outputFileResults) {
-                                Toast.makeText(MainActivity.this, "Video has been saved successfully.", Toast.LENGTH_SHORT).show();
-                            }
+                    videoCapture.startRecording(
+                            new VideoCapture.OutputFileOptions.Builder(
+                                    getContentResolver(),
+                                    MediaStore.Video.Media.EXTERNAL_CONTENT_URI,
+                                    contentValues
+                            ).build(),
+                            getExecutor(),
+                            new VideoCapture.OnVideoSavedCallback() {
+                                @Override
+                                public void onVideoSaved(@NonNull VideoCapture.OutputFileResults outputFileResults) {
+                                    Toast.makeText(MainActivity.this, "Video has been saved successfully.", Toast.LENGTH_SHORT).show();
+                                }
 
-                            @Override
-                            public void onError(int videoCaptureError, @NonNull String message, @Nullable Throwable cause) {
-                                Toast.makeText(MainActivity.this, "Error saving video: " + message, Toast.LENGTH_SHORT).show();
+                                @Override
+                                public void onError(int videoCaptureError, @NonNull String message, @Nullable Throwable cause) {
+                                    Toast.makeText(MainActivity.this, "Error saving video: " + message, Toast.LENGTH_SHORT).show();
+                                }
                             }
-                        }
-                );
+                    );
             } catch (Exception e) {
                 e.printStackTrace();
             }
@@ -207,7 +243,6 @@ public class MainActivity extends AppCompatActivity implements ImageAnalysis.Ana
         ContentValues contentValues = new ContentValues();
         contentValues.put(MediaStore.MediaColumns.DISPLAY_NAME, timestamp);
         contentValues.put(MediaStore.MediaColumns.MIME_TYPE, "image/jpeg");
-
 
 
         imageCapture.takePicture(
